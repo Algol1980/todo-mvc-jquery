49f5205a3281303587470389ddb369f5146e6c24
algol1980 <algol1980@yandex.ru>
2015-09-14 14:26:54 +0300

    sage aborts the commit.

---
+++ js/main.js
@@ -0,0 +1,82 @@
+$(function() {
+
+  var $send = $('#form'),
+    $message = $('.form-control'),
+    $list = $('.list.list-unstyled'),
+    $chkBox = $(".itemToDo"),
+    $todoCount = $(".uncomplete").size();
+
+
+  function todoCnt() {
+    $todoCount = $(".uncomplete").size();
+    $(".todo-count").text($todoCount + "  дел осталось выполнить");
+
+  };
+
+
+  $send.on("submit", $send, function(e) {
+    e.preventDefault();
+    console.log('Was clicked');
+
+    var mes = $message.val();
+    $message.val('');
+
+    $list.append('<li><label><input class="itemToDo uncomplete" type="checkbox"> ' + mes + '</label></li>');
+    todoCnt();
+
+
+  });
+
+  $(".main").on("click", ".itemToDo", function() {
+    if ($(this).is(":checked")) {
+      $(this).toggleClass('completed').toggleClass('uncomplete');
+      $(this).parent("label").css({
+        "text-decoration": "line-through",
+        color: "gray"
+      });
+    } else {
+      $(this).toggleClass('completed').toggleClass('uncomplete');
+      $(this).parent("label").css({
+        "text-decoration": "",
+        color: "#000"
+      });
+    }
+    todoCnt();
+  });
+
+  $('#allComplete').on('change', function() {
+    if ($(this).is(':checked')) {
+      $('.itemToDo').prop('checked', $(this).is(':checked')).toggleClass('completed').parent("label").css({
+        "text-decoration": "line-through",
+        color: "gray"
+      });
+    } else {
+      $('.itemToDo').removeAttr('checked').toggleClass('completed').parent("label").css({
+        "text-decoration": "",
+        color: "#000"
+      });
+    }
+    todoCnt();
+  });
+
+
+  $(".selected").click(function() {
+    $('.itemToDo').parent().parent().toggle();
+  });
+
+  $("a[href='#/complited']").click(function() {
+    $('.uncomplete').parent().parent().toggle();
+  });
+
+  $("a[href='#/active']").click(function() {
+    $('.completed').parent().parent().toggle();
+  });
+
+  $(".clear-complited").click(function() {
+    $('.completed').parent().parent().remove();
+    todoCnt();
+  });
+
+  $(".todo-count").text($todoCount + "  дел осталось выполнить");
+
+});
